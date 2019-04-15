43f84afcdda2bd101a74bfb8de8c533f1a52b4c7
Aman Gupta <vicky.amangupta@gmail.com>
2019-04-15 05:35:27 +0530

    all previous work updated

--- layouts/PICKPACK/js/tab_js.js
+++ layouts/PICKPACK/js/tab_js.js
@@ -6,19 +6,36 @@
 		  	$('.pagination').html('');						// reset pagination div
 		  	var trnum = 0 ;									// reset tr counter 
 		  	var maxRows = parseInt($(this).val());			// get Max Rows from select option
-        
-		  	var totalRows = $(table+' tbody tr').length;		// numbers of rows 
-			 $(table+' tr:gt(0)').each(function(){			// each TR in  table and not the header
-			 	trnum++;									// Start Counter 
-			 	if (trnum > maxRows ){						// if tr number gt maxRows
+		  	var totalRows = $(table+' tbody tr').length;
+		  	var pagenum = Math.ceil(totalRows/maxRows);
+
+		  // 			// numbers of rows 
+			 // $(table+' tr:gt(0)').each(function(){			// each TR in  table and not the header
+			 // 	trnum++;									// Start Counter 
+			 // 	if (trnum > maxRows ){						// if tr number gt maxRows
 			 		
-			 		$(this).hide();							// fade it out 
-			 	}if (trnum <= maxRows ){$(this).show();}// else fade in Important in case if it ..
-			 });
+			 // 		$(this).hide();							// fade it out 
+			 // 	}if (trnum <= maxRows ){$(this).show();}// else fade in Important in case if it ..
+			 // });
+       				
+			 var trIndex = 0 ;
+			$(table+' tr:gt(0)').each(function(){    // each tr in table not the header
+          trIndex++;       // tr index counter 
+          // if tr index gt maxRows*pageNum or lt maxRows*pageNum-maxRows fade if out
+          if (trIndex >((maxRows*pagenum)-maxRows)){
+            $(this).show();   
+          }else {$(this).hide();} 
+                   //else fade in 
+         });                    // end of for each tr in table
+
+
+
+
+
        											//  was fade out to fade it in 
 			 if (totalRows > maxRows)
-      {						// if tr total rows gt max rows option
-			 	var pagenum = Math.ceil(totalRows/maxRows);	// ceil total(rows/maxrows) to get ..  
+      		{						// if tr total rows gt max rows option
+						// ceil total(rows/maxrows) to get ..  
 			 												//	numbers of pages 
 			  	for (var i = 1; i <= pagenum ;){			// for each page append pagination li 
 			 	$('.pagination').append('<li data-page="'+i+'">\
@@ -36,26 +53,26 @@
 // .............................DEFAULT PAGE SHOWING START...............................................
 
 
-        var pageNum = pagenum;
-        var trIndex = 0 ;             // reset tr counter
-       // $('.pagination li').removeClass('active');  // remove active class from all li 
-       // $(this).addClass('active');         // add active class to the clicked 
+       //  var pageNum = pagenum;
+       //  var trIndex = 0 ;             // reset tr counter
+       // // $('.pagination li').removeClass('active');  // remove active class from all li 
+       // // $(this).addClass('active');         // add active class to the clicked 
         
         
-        //SHOWING ROWS NUMBER OUT OF TOTAL
-       showig_rows_count(maxRows, pageNum, totalRows);
-        //SHOWING ROWS NUMBER OUT OF TOTAL
+       //  //SHOWING ROWS NUMBER OUT OF TOTAL
+       // showig_rows_count(maxRows, pageNum, totalRows);
+       //  //SHOWING ROWS NUMBER OUT OF TOTAL
         
         
         
-         $(table+' tr:gt(0)').each(function(){    // each tr in table not the header
-          trIndex++;       // tr index counter 
-          // if tr index gt maxRows*pageNum or lt maxRows*pageNum-maxRows fade if out
-          if (trIndex >((maxRows*pageNum)-maxRows)){
-            $(this).show();   
-          }else {$(this).hide();} 
-                   //else fade in 
-         });                    // end of for each tr in table
+       //   $(table+' tr:gt(0)').each(function(){    // each tr in table not the header
+       //    trIndex++;       // tr index counter 
+       //    // if tr index gt maxRows*pageNum or lt maxRows*pageNum-maxRows fade if out
+       //    if (trIndex >((maxRows*pageNum)-maxRows)){
+       //      $(this).show();   
+       //    }else {$(this).hide();} 
+       //             //else fade in 
+       //   });                    // end of for each tr in table
 
 
 
@@ -68,12 +85,25 @@
 
 
       											// end if row count > max rows
-			$('.pagination li:last-child').addClass('active'); // add active class to the first li 
+$('.pagination li:last-child').addClass('active'); // add active class to the first li 
+
+
+
         
         
         //SHOWING ROWS NUMBER OUT OF TOTAL DEFAULT
       // showig_rows_count(maxRows, pagenum, totalRows);
         //SHOWING ROWS NUMBER OUT OF TOTAL DEFAULT
+
+
+
+
+        	showig_rows_count(maxRows, pagenum, totalRows);
+
+
+
+
+
 
         $('.pagination li').on('click',function(e){		// on click each page
         e.preventDefault();
@@ -82,10 +112,10 @@
 				$('.pagination li').removeClass('active');	// remove active class from all li 
 				$(this).addClass('active');					// add active class to the clicked 
         
-        
-        //SHOWING ROWS NUMBER OUT OF TOTAL
-       showig_rows_count(maxRows, pageNum, totalRows);
-        //SHOWING ROWS NUMBER OUT OF TOTAL
+			        //SHOWING ROWS NUMBER OUT OF TOTAL
+			       showig_rows_count(maxRows, pageNum, totalRows);
+			        //SHOWING ROWS NUMBER OUT OF TOTAL
+       
         
         
         
@@ -124,16 +154,16 @@ function showig_rows_count(maxRows, pageNum, totalRows) {
 }
 
 // CREATING INDEX
-function default_index() {
-  $('table#table-id tr:eq(0)').prepend('<th> Item_No </th>')
+// function default_index() {
+//   $('table#table-id tr:eq(0)').prepend('<th> Item_No </th>')
 
-					var id = 0;
+// 					var id = 0;
 
-					$('table#table-id tr:gt(0)').each(function(){	
-						id++
-						$(this).prepend('<td>'+id+'</td>');
-					});
-}
+// 					$('table#table-id tr:gt(0)').each(function(){	
+// 						id++
+// 						$(this).prepend('<td>'+id+'</td>');
+// 					});
+// }
 
 // All Table search script
 function FilterkeyWord_all_table() {
